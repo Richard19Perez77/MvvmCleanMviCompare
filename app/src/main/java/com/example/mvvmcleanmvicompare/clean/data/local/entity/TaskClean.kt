e0a3172e0694e842e58345b27f77c41cb6248102
rick <richard19.perez77@yahoo.com>
2025-04-04 12:28:45 -0500

    update test for view models

--- app/src/main/java/com/example/mvvmcleanmvicompare/clean/data/local/entity/TaskClean.kt
+++ app/src/main/java/com/example/mvvmcleanmvicompare/clean/data/local/entity/TaskClean.kt
@@ -11,6 +11,25 @@ data class TaskClean(
     val isDone: Boolean = false
 )
 
+/**
+ * TaskClean will have conversion methods to call for converting from Task to TaskClean and vice versa.
+ *
+ *  Why?
+ *      Clean separates architecture layers
+ *      Data  - TaskClean (@Entity) for Room and DB only
+ *      Domain - Task for core business logic
+ *      Presentation TaskUiState for UI only view state
+ *
+ *      Because each layer is independent, we need mappers to convert between them.
+ *      // Data -> Domain
+ *      fun TaskClean.toDomain(): Task = Task(id = id, title = title, isDone = isDone)
+ *
+ *      // Domain -> Data
+ *      fun Task.toEntity(): TaskClean = TaskClean(id = id, title = title, isDone = isDone)
+ *
+ *      This keeps each layer clean, reusable, and testable - no db imports in domain layer, and domain is database agnostic.
+ */
+
 fun TaskClean.toDomain(): Task = Task(id = id, title = title, isDone = isDone)
 
 fun Task.toEntity(): TaskClean = TaskClean(id = id, title = title, isDone = isDone)
